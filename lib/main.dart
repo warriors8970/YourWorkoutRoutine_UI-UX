5ac8fd3de3ad5120978e27947ca99612f5cddcf4
warriors8970 <warriors8970@gmail.com>
2023-05-22 12:07:55 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:fitness/calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 
 
 void main() => runApp(MaterialApp(
@@ -24,25 +26,40 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-
         body:screens[currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.black,
-          showUnselectedLabels: false,
-            currentIndex: currentIndex,
-            onTap: (index) => setState(() =>currentIndex = index),
-            items:const [
-              BottomNavigationBarItem(icon: Icon(Icons.home),
-                  label: 'Home'),
-              BottomNavigationBarItem(icon: Icon(Icons.leaderboard_rounded),
-                  label: 'fav'),
-              BottomNavigationBarItem(icon: Icon(Icons.calendar_month),
-                  label: 'person'),
-              BottomNavigationBarItem(icon: Icon(Icons.person),
-                  label: 'person')
-            ] ),
-      ),
-    );
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: RenderErrorBox.backgroundColor = Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(24))
+            
+          ),
+          child: BottomNavigationBar(
+
+            unselectedFontSize: 0.0,
+              selectedFontSize: 0.0,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              selectedItemColor:Colors.black,
+              showUnselectedLabels: false,
+              elevation: 0,
+              currentIndex: currentIndex,
+              onTap: (index) => setState(() =>currentIndex = index),
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home),
+                    label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.leaderboard_rounded),
+                    label: 'fav'),
+                BottomNavigationBarItem(icon: Icon(Icons.calendar_month),
+                    label: 'person'),
+                BottomNavigationBarItem(icon: Icon(Icons.person,shadows: [],),
+                    label: 'person'),
+              ] ),
+        ),
+        ),
+      );
+
   }
 }
 
